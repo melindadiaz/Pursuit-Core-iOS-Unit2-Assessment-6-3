1104b63481d7c777eb1c640e83d1668cf59a2e7d
melinda <melindad_82@msn.com>
2019-11-22 12:10:05 -0500

    Started tableview form and cells

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    
+    @IBOutlet weak var crayonTableView: UITableView!
+    
+    let crayonReference = Crayon.allTheCrayons
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    crayonTableView.dataSource = self
+    crayonTableView.delegate = self
+    
   }
 
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crayonReference.count
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CrayonColors", for: indexPath)
+        let crayons = crayonReference[indexPath.row]
+        
+        //cell.textLabel?.text = crayons.name[indexPath.row]
+        return cell
+       }
+    
 }
 
